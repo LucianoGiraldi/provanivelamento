57e8defe0a21f6daeca221a9a7b16cb760a6bf2c
LucianoGiraldi <luciano.texugo@gmail.com>
2025-09-16 19:10:45 -0300

    auth e user delete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { AuthModule } from "./auth/auth.module";
-import { UsersModule } from "./users/users.module";
-import { CursoModule } from "./curso/curso.module";
-import { DisciplinaModule } from "./disciplina/disciplina.module";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CursoModule } from './curso/curso.module';
+import { DisciplinaModule } from './disciplina/disciplina.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, CursoModule, DisciplinaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CursoModule,
+    DisciplinaModule,
+  ],
 })
 export class AppModule {}
